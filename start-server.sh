f5baf7bdbc613c61b5dac470228cc317454cb3e7
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-02-08 13:43:17 +0700

    Changed settings and server startup script

--- start-server.sh
+++ start-server.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd REST; python manage.py createsuperuser --no-input)
 fi
-(cd REST; gunicorn REST.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd REST; gunicorn REST.wsgi --user www-data --bind 0.0.0.0:8011 --workers 3) &
 nginx -g "daemon off;"
